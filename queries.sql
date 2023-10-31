d9fc35a44159a2d616a57eb1e142ff3a125e09a7
Jialing Liu <jialingl.liu@gmail.com>
2023-10-30 21:10:08 -0400

    added more to queries sql part 1.

--- queries.sql
+++ queries.sql
@@ -1,7 +1,9 @@
 --Part 1
 
 --SELECT * FROM job; (to show entire table)
---SHOW COLUMNS FROM job; (to check data type of each column in the job table)
+--SHOW COLUMNS FROM job; (to check data type of each column in the job table op 1)
+--DESCRIBE job; (to check data type of each column in the job table op 2)
+--SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'job'; (to check data type of each column and more in the job table op 3)
 
 --Part 2
 
